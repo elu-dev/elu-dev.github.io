850c22aba0b3df696bbe9211a66b4b01f9e66e21
Edgar Lugo <elu.developer@gmail.com>
2020-01-07 05:03:35 +0100

    move the public files to root

--- minify-src.py
+++ minify-src.py
@@ -8,6 +8,7 @@ APIS = {
 }
 
 for (current_path, _, files) in os.walk('./src'):
+    if 'assets' in current_path: continue
     for filename in files:
         print(f'minimizing {filename}...')
         fullpath = f'{current_path}/{filename}'
@@ -19,7 +20,7 @@ for (current_path, _, files) in os.walk('./src'):
         with urllib.request.urlopen(req) as res:
             new_data = res.read().decode('utf-8')
             if filename[-2:] == 'ml': new_data = new_data.replace('\n','')
-            publicpath = fullpath.replace('src','public', 1)
+            publicpath = fullpath.replace('src/','', 1)
 
             with open(publicpath, 'w') as f: f.write(new_data)
 
